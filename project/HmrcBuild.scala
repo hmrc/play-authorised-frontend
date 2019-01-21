bc651b1842026d87548a8e300a5a66c8df5015b8
yuan zhang <yuanzhang910@hotmail.com>
2019-01-21 16:23:05 +0000

    AIV-604: to the new buildd

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -1,24 +1,25 @@
 import sbt.Keys._
 import sbt._
+import uk.gov.hmrc.{SbtArtifactory, SbtAutoBuildPlugin}
+import uk.gov.hmrc.versioning.SbtGitVersioning
+import uk.gov.hmrc.versioning.SbtGitVersioning.autoImport.majorVersion
+
 
 object HmrcBuild extends Build {
-
-  import uk.gov.hmrc._
-  import uk.gov.hmrc.versioning.SbtGitVersioning
 
   val appName = "play-authorised-frontend"
 
   lazy val library = (project in file("."))
-    .enablePlugins(SbtAutoBuildPlugin, SbtGitVersioning)
+    .enablePlugins(SbtAutoBuildPlugin, SbtGitVersioning, SbtArtifactory)
     .settings(
       name := appName,
       scalaVersion := "2.11.7",
       crossScalaVersions := Seq("2.11.7"),
-      libraryDependencies ++= AppDependencies(),
-      resolvers := Seq(
-        Resolver.bintrayRepo("hmrc", "releases"),
-        "typesafe-releases" at "http://repo.typesafe.com/typesafe/releases/"
-      )
+      libraryDependencies ++= AppDependencies()
+    )
+    .settings(majorVersion := 7)
+    .settings(resolvers += Resolver.bintrayRepo("hmrc", "releases"),
+      resolvers += "hmrc-releases" at "https://artefacts.tax.service.gov.uk/artifactory/hmrc-releases/"
     )
 }
 
